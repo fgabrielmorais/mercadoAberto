138421d152a2f1bf76dff73c7390538b6133d4d1
Gabriel <gabrielgamer.aleatoriow@gmail.com>
2022-07-02 12:59:31 -0300

    Gabriel

--- lib/main.dart
+++ lib/main.dart
@@ -45,8 +45,8 @@ class _MyHomePageState extends State<MyHomePage> {
   
     return Scaffold(
       appBar: AppBar(
-
-      
+      elevation: 0,
+     backgroundColor: Color(0xfff5d415),
        leading: Builder(builder: (BuildContext context){
        return IconButton(
             icon: const Icon(Icons.menu_outlined, color: Colors.black),
@@ -57,49 +57,79 @@ class _MyHomePageState extends State<MyHomePage> {
             },
        );
        },
-       ), 
-
-       backgroundColor: Color(0xfff5d415),
-        title: TextField( 
+       ),
+       
+          actions: <Widget>[
+          Container(
+            width: MediaQuery.of(context).size.width - 
+            (2 * 32) - (8 * 5),
+            padding: const EdgeInsets.only(top: 9),
+              child: TextField( 
               decoration: InputDecoration(
+              isDense: true,
+              prefixIcon: const Icon(Icons.search),
               contentPadding: EdgeInsets.symmetric(vertical: 5.0),
               filled: true,
               fillColor: Colors.white,
               border: OutlineInputBorder(
-                 borderRadius: BorderRadius.circular(50.0),
+              borderRadius: BorderRadius.circular(50.0),
               ),
-              hintText: 'Enter your email'
+              hintText:
+               'Buscar no Mercado Aberto'
               ),
               ),
-          actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.shopping_cart_checkout_outlined, color: Colors.black),
-            tooltip: 'Show Snackbar',
+          ),
+          const Padding(padding: EdgeInsets.all(5)),
+           Container(child: IconButton(
+            icon: const Icon(Icons.shopping_cart_outlined, color: Colors.black),
+            tooltip: 'Oii :)',
             onPressed: () {
               ScaffoldMessenger.of(context).showSnackBar(
                   const SnackBar(content: Text('This is a snackbar')));
             },
           ),
-      ],
-    
+           ),
+          ],
+      bottom: const PreferredSize(
 
+
+        preferredSize: Size.fromHeight(50),
+        child: ListTile(leading: 
+        Icon(Icons.pin_drop_outlined, color: Colors.black, size: 19.0,),
+        
+        title: 
+        Text("Enviar para Marcello Queiroz - Rua Jardim Paulista, 56", 
+        style: TextStyle(fontSize: 11.5), textAlign: TextAlign.center)
+        ),
+  
+      ),
+    
       ),
       body: Center(
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin:  Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              stops: [-15.0, 0.1],
+              colors: [ Color(0xfff5d415), Colors.white ],
+            )
+          ),
+
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Text(
               '$_counter',
+              textAlign: TextAlign.center,
               style: Theme.of(context).textTheme.headline4,
             ),
           ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+        ),  
+    
+    )
+      )
     );
   }
 }
