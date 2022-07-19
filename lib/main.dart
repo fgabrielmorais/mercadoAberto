9d1408ec1424f4ec7dc7665f950b2caa2466e817
gabrielgamer.aleatoriow@gmail.com <gabrielgamer.aleatoriow@gmail.com>
2022-07-19 20:16:10 -0300

    Códigos feitos na aula de sábado (16/07)

--- lib/main.dart
+++ lib/main.dart
@@ -71,7 +71,12 @@ class _MyHomePageState extends State<MyHomePage> {
               contentPadding: EdgeInsets.symmetric(vertical: 5.0),
               filled: true,
               fillColor: Colors.white,
-              border: OutlineInputBorder(
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.white, width: 1.0),
+                borderRadius: BorderRadius.circular(50.0),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.white, width: 1.0),
               borderRadius: BorderRadius.circular(50.0),
               ),
               hintText:
@@ -105,31 +110,92 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     
       ),
-      body: Center(
-        child: Container(
+      body:
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        
+        children: [
+          Container(
           decoration: BoxDecoration(
+            
             gradient: LinearGradient(
               begin:  Alignment.topCenter,
               end: Alignment.bottomCenter,
-              stops: [-15.0, 0.1],
+              stops: [-84.0, 0.92],
               colors: [ Color(0xfff5d415), Colors.white ],
             )
           ),
+          child: Padding(padding: const EdgeInsets.only(
+              left: 20, right: 20, top: 80),
+              child: Image.asset(
+                "assets/images/setedosete.png",
+                width: 352.67,
+                height: 109.14,
+                alignment: Alignment.topCenter
+          )),
+        
+    ),  
+    SizedBox(
+      height: 15,
+    ),
+     Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+ Container(
+       decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5.0),
+            gradient: LinearGradient(
+              begin:  Alignment.topRight,
+              end: Alignment.topLeft,
+              stops: [-10.0, 0.9],
+              colors: [ Colors.pinkAccent, Colors.purple ],
+            )
+          ),
+      height: 30.0,
+      width: 374.0,
+          child: Row(
+             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+             children: [
+             
+              Text("  Assinatura por R\$9,90/mês",   style: TextStyle(fontSize: 16, color: Colors.white,)),
+               Icon(Icons.arrow_forward_ios_rounded,  color: Colors.white, size: 19.0,),
+             ],
+          ),
+          
+    ),
 
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Text(
-              '$_counter',
-              textAlign: TextAlign.center,
-              style: Theme.of(context).textTheme.headline4,
-            ),
+     SizedBox(
+      height: 8,
+    ),
+
+     Container(
+      width: 374.0,
+      height: 30.0,
+      
+       decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5.0),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey,
+              blurRadius: 4,
+              offset: Offset(3, 5),
+
+              ),
           ],
-        ),  
-    
+          ),
+          child: Row(
+             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+             children: [
+              Icon(Icons.car_repair_rounded,  color: Colors.grey, size: 27.0,),
+              Text("Frete grátis em todo o Brasil",   style: TextStyle(fontSize: 16, color: Colors.green,), textAlign: TextAlign.left,)
+             ],
+          ),
     )
-      )
+      ],
+     ),
+        ],
+      ),
     );
   }
 }
